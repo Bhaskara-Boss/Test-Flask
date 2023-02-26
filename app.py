e1940969185a559e0aae974fc4185b03324eda72
Boss Self <119592716+Bhaskara-Boss@users.noreply.github.com>
2023-02-26 10:36:50 +0000

    Html and URl

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask (__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home_page():
+    return render_template('index.html') 
+
+@app.route("/books")
+def books_store(): 
+	return render_template('books.html')
